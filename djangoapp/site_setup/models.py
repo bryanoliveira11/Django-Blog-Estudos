3c1847b4b7d0b1bfba40f3da9196b3df14d688fd
bryan <bryan.oliveira12345@gmail.com>
2023-05-22 15:00:39 -0300

    utils, context-processor and style

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 
 class MenuLink(models.Model):
@@ -33,6 +35,22 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default='',
+        validators=[validate_png],
+    )
+
+    def save(self, *args, **kwargs):
+        current_faveicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_faveicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
